76294b48dddb68f1bd0bb0c081b4f68a494be0d1
dior <diorrego1@gmail.com>
2022-08-31 04:28:22 -0400

    :heavy_plus_sign: add testing-library react and refactor Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import Button from './';
+import Button from './Button';
 
 const setup = () => render(<Button disabled>disabled</Button>);
 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Text from '../Text';
+
+const Button = () => {
+
+  return (
+    <button></button>
+  );
+};
+
+export default Button;
