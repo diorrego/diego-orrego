350c20be7c86790149367d799bddb4660f06150c
dior <diorrego1@gmail.com>
2022-08-31 16:39:14 -0400

    :construction: config testing enviroment

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import Button from './Button';
 
-const setup = () => render(<Button disabled>disabled</Button>);
+const setup = () => render(<Button>button</Button>);
+
 
 it('should render a disabled button', () => {
   setup();
-  const byElement = screen.getByRole('button', { name: /disabled/i });
+  const byElement = screen.getByRole('button', { name: /button/i });
 
   expect(byElement).toBeInTheDocument();
-  expect(byElement).toHaveClass('bg-gray-500');
-  expect(byElement).toBeDisabled();
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Text from '../Text';
 
-const Button = () => {
+type ButtonProps = {
+  children: string;
+}
 
+const Button = ({children}: ButtonProps) => {
   return (
-    <button></button>
+    <button>{children}</button>
   );
 };
 
